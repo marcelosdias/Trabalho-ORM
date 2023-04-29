508e80bf189b997d9fac6d6f33f9c2b3f2aeb2f8
marcelosdias <marcelo.sdias@inf.ufpel.edu.br>
2023-04-29 15:36:57 -0300

    criando o resto das migratons

--- src/database/migrations/20230425010128_initial.js
+++ src/database/migrations/20230425010128_initial.js
@@ -23,10 +23,59 @@ exports.up = (knex) => knex.schema
       .references('id')
       .inTable('categories')
       .onDelete('CASCADE');
+  })
+
+  .createTable('companies', (table) => {
+    table.uuid('id').unique().notNullable().primary();
+    table.string('name').unique().notNullable();
+    table.string('email').unique().notNullable();
+    table.string('password').notNullable();
+    table.string('city').notNullable();
+    table.string('address').notNullable();
     table.timestamps(true, true);
+  })
+
+  .createTable('jobs', (table) => {
+    table.uuid('id').unique().notNullable().primary();
+    table.string('title').notNullable();
+    table.string('description').notNullable();
+    table.uuid('company_id')
+      .references('id')
+      .inTable('companies')
+      .onDelete('CASCADE');
+    table.integer('category_id')
+      .references('id')
+      .inTable('categories')
+      .onDelete('CASCADE');
+    table.timestamps(true, true);
+  })
+
+  .createTable('candidates_jobs', (table) => {
+    table.increments('id').unique().notNullable().primary();
+    table.uuid('job_id')
+      .references('id')
+      .inTable('jobs')
+      .onDelete('CASCADE');
+    table.uuid('candidate_id')
+      .references('id')
+      .inTable('candidates')
+      .onDelete('CASCADE');
+    table.date('date').notNullable();
+  })
+
+  .createTable('interviews', (table) => {
+    table.uuid('id').unique().notNullable().primary();
+    table.integer('candidates_jobs_id')
+      .references('id')
+      .inTable('candidates_jobs')
+      .onDelete('CASCADE');
   });
 
 exports.down = (knex) => knex.schema
+  .dropTableIfExists('intervies')
+  .dropTableIfExists('candidates_jobs')
+  .dropTableIfExists('jobs')
+  .dropTableIfExists('companies')
   .dropTableIfExists('candidates_categories')
   .dropTableIfExists('categories')
   .dropTableIfExists('candidates');
