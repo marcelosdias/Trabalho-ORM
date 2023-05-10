fd51241f341b9fa5e25b349ea9660c3c3954ce9c
Marcelo Ribeiro <mcribeiro@inf.ufpel.edu.br>
2023-05-10 19:27:55 -0300

    feat: final implementations

--- src/controllers/InterviewController.js
+++ src/controllers/InterviewController.js
@@ -10,8 +10,8 @@ class InterviewController {
 
     return response.json(interviews);
   }
-
-  async create(request, response) {
+  
+  async store(request, response) {
     const { candidates_jobs_id, date } = request.body;
 
     const interview = await Interview.query().insert({
@@ -20,6 +20,39 @@ class InterviewController {
     });
 
     return response.json(interview);
+  }
+
+  // Listar uma entrevista pelo ID dela
+  async show(request, response) {
+    const { id } = request.params;
+
+    const interview = await Interview.query()
+      .findById(id);
+
+    return response.json(interview);
+  }
+  // Atualizar entrevista
+  async update(request, response) {
+    const { status, date } = request.body;
+
+    const { id } = request.params;
+
+    const updatedInterview = await Interview.query().patchAndFetchById(id, {
+      status, date
+    });
+
+    return response.json(updatedInterview);
+  }
+
+  // Deletar entrevista
+  async delete(request, response) {
+    const { id } = request.params;
+
+    const isDeleted = await Interview.query().deleteById(id);
+
+    if (!isDeleted) { return response.status(404).json({ message: 'Entrevista não encontrada' }); }
+
+    return response.json({ message: 'Entrevista deletada' });
   }
 }
 
--- src/routes/jobRoutes.js
+++ src/routes/jobRoutes.js
@@ -4,7 +4,11 @@ const JobController = require('../controllers/JobController');
 const companyRouter = Router();
 
 companyRouter.get('/jobs', JobController.index);
+companyRouter.get('/jobs/:id', JobController.show);
 companyRouter.get('/jobs/categories/:id', JobController.filteredJobs);
 companyRouter.get('/jobs/candidate/:id', JobController.filterJobsForCandidate);
+companyRouter.put('/jobs/:id', JobController.update);
+companyRouter.delete('/jobs:id', JobController.delete);
+
 
 module.exports = companyRouter;
